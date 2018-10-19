85a832ec0506d68245429b4798a2365e4c815f76
rsabaric <sabaricr@seattleu.edu>
2018-10-18 23:15:26 -0700

    a few stretch goals

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -2,28 +2,28 @@
   <div class="results">
     <h1>Top Movies from the 20<sup>th</sup> Century</h1>
     <p class="search-meta">
-      <span class="current-page"><b>Current Page:</b>1{{ page }}</span>
+      <span class="current-page"><b>Current Page:</b>{{ page }}</span>
       <span class="total-pages"><b>Pages:</b>{{ total_pages }}</span>
       <span class="total-results"><b>Count:</b>{{ total_results }}</span>
     </p>
 
     <ul>
-      <li class="movie-item" v-for="result in results">
-        <img v-bind:src="'https://image.tmdb.org/t/p/w150_and_h225_bestv2' + result.poster_path" v-bind:alt="results.title" class="poster-image">
-        <h2 class="title"><a v-bind:href="'https://www.themoviedb.org/movie/'+ result.id">{{ result.title }}</a></h2>
-        <div class="ratings">
-          <span class="rating-category critics-choice" v-if="result.vote_average > 8">Critic's Choice</span>
-          <span class="rating-category well-liked" v-else-if="result.vote_average > 7">Well Liked</span>
-          <span class="rating-category stinker" v-else>Stinker</span>
-          <span class="vote-average">{{ result.vote_average }}</span> with <span class="vote-count">{{result.vote_count}}</span> votes
-        </div>
-        <p class="overview">
-          {{ result.overview}}
-        </p>
-        <p class="release-date">Original Release: {{ result.release_date}}</p>
-        <ul class="genre-list"><!-- TODO: Use a loop to iterate through all of the genres for this movie. -->
-          <li v-for="genre in result.genres">{{genre}}</li><!-- TODO: Fill in the genre name from the data. -->
-        </ul>
+      <li class="movie-item" v-for="result in results" v-bind:style="{ backgroundImage: 'url(https://image.tmdb.org/t/p/w150_and_h225_bestv2' + result.backdrop_path +')'}">
+          <img v-bind:src="'https://image.tmdb.org/t/p/w150_and_h225_bestv2' + result.poster_path" v-bind:alt="results.title" class="poster-image">
+          <h2 class="title"><a v-bind:href="'https://www.themoviedb.org/movie/'+ result.id">{{ result.title }}</a></h2>
+          <div class="ratings">
+            <span class="rating-category critics-choice" v-if="result.vote_average > 8">Critic's Choice</span>
+            <span class="rating-category well-liked" v-else-if="result.vote_average > 7">Well Liked</span>
+            <span class="rating-category stinker" v-else>Stinker</span>
+            <span class="vote-average">{{ result.vote_average }}</span> with <span class="vote-count">{{result.vote_count}}</span> votes
+          </div>
+          <p class="overview">
+            {{ result.overview }}
+          </p>
+          <p class="release-date">Original Release: {{ result.release_date | formatDate}}</p>
+          <ul class="genre-list"><!-- TODO: Use a loop to iterate through all of the genres for this movie. -->
+            <li v-for="genre in result.genres">{{genre}}</li><!-- TODO: Fill in the genre name from the data. -->
+          </ul>
       </li>
     </ul>
   </div>
@@ -35,8 +35,15 @@ export default {
   name: 'results',
   data () {
     return apiresults
+  },
+  filters: {
+    formatDate: function (value) {
+      value = new Date(value)
+      return value.toDateString()
+    }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -57,6 +64,10 @@ ul {
   margin: 10px 0;
   padding: 2rem;
   box-shadow: 4px 4px 10px rgba(0,0,0,0.2);
+  background-repeat: repeat y;
+  background-position: center; 
+  background-blend-mode:overlay;
+  background-color:bisque;
 }
 
 .movie-item img {
